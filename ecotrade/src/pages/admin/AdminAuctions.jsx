091da871723d457e1446ca069d50ceccac63d420
anikethintech <aniketh.v@intechadditive.com>
2026-02-09 13:04:32 +0530

    Fix: Remove incorrect closing fragment tag causing build error

--- ecotrade/src/pages/admin/AdminAuctions.jsx
+++ ecotrade/src/pages/admin/AdminAuctions.jsx
@@ -1031,7 +1031,7 @@ const AdminAuctions = () => {
                       )}
                     </div>
                   ))}
-                    </>
+                    </div>
                   )}
                 </div>
               )}
